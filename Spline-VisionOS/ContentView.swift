5aa6ea62f59f16b5714ce8492832ea64a6235183
setouchi-h <setouchi.kazuki@gmail.com>
2024-09-23 18:19:34 +0900

    Initial Commit

---
+++ Spline-VisionOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Spline-VisionOS
+//
+//  Created by 橋本一輝 on 2024/09/23.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ Spline-VisionOS/Spline_VisionOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spline_VisionOSApp.swift
+//  Spline-VisionOS
+//
+//  Created by 橋本一輝 on 2024/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Spline_VisionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
